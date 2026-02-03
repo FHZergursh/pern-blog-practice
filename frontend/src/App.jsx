13cb04d4939f294fa1c5c2a340bae8d6782fd434
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-02-03 23:39:11 +0000

    add simple header
    title, will expand later, just wanna PR so I can work on another device tomorrow

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
+import Header from './components/Header.jsx'
 
 const App = () => {
   return (
-    <div>
+    <div className='bg-slate-700 min-h-screen'>
+      <Header />
     <Routes>
       <Route path="/" element={<HomePage />} />
     </Routes>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-
+    <div class="flex flex-col items-center">
+      <div class="flex bg-blue-300 w-2/3 h-50 mb-5 mt-50 ">
+        <h1 class="mb-10 text-6xl font-bold font-lato ">Message Here</h1>
+      </div>
     </div>
+
   )
 }
 
